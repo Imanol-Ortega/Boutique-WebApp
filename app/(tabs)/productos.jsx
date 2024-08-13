b5cd27c2db61f6e2b8e5e08ba747b9d342122da0
Imanol-Ortega <imanicoortemohr018@gmail.com>
2024-08-13 15:52:06 -0400

    a

--- app/(tabs)/productos.jsx
+++ app/(tabs)/productos.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { View, Text, StatusBar, FlatList, Image } from "react-native";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
